84ee806e3f9ce944950daa0f6cc732e5e7ac8826
FelixSundset <felsun@live.se>
2022-11-23 13:11:19 +0100

    Changes

--- felsun-rps/src/parkingGarage/Garage.java
+++ felsun-rps/src/parkingGarage/Garage.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Garage {
 
     public ArrayList<Vehicle> slots = new ArrayList<Vehicle>();
-
+    public ArrayList<Vehicle> park = new ArrayList<Vehicle>();
 
     public void parkVehicle() {
     }
@@ -26,7 +26,7 @@ public class Garage {
         Car car2 = new Car(2017, "Gold", "LGE 563", 4);
         Airplane airplane1 = new Airplane(1956, "Black", "ASD 432", 2);
         Airplane airplane2 = new Airplane(1993, "Orange", "SGT 746", 2);
-        Motorcycle motorcycle1 = new Motorcycle(2010, "Blue", "123412", 4);
+        Motorcycle motorcycle1 = new Motorcycle(2010, "Blue", "LKJ 872", 4);
         Motorcycle motorcycle2 = new Motorcycle(2022, "White", "MGJ 923", 2);
         Boat boat1 = new Boat(2010, "Blue", "OJF 321", 1);
         Boat boat2 = new Boat(1989, "Purple", "LAS 912", 2);
@@ -34,6 +34,10 @@ public class Garage {
         slots.add(boat1);
         slots.add(airplane1);
         slots.add(motorcycle1);
+        park.add(car2);
+        park.add(boat2);
+        park.add(airplane2);
+        park.add(motorcycle2);
     }
 
     public void inputMenu() {
@@ -48,21 +52,38 @@ public class Garage {
 
         if (choice == 1) {
             System.out.println("Vilket fordon vill du parkera?");
-
+            System.out.println(park);
+            scanner.nextLine();
+            System.out.println(scanner.nextLine());
+            System.out.println("Fordonet är nu parkerat");
         }
         if (choice == 2) {
-            System.out.println("Vilket fordon vill du hämta?");
+            System.out.println("Vilket fordon vill du hämta? ");
+            System.out.println(slotsToString());
+            scanner.nextLine();
+            System.out.println("Vill du hämta: " +scanner.nextLine()+ "?");
+            scanner.nextLine();
+            System.out.println("Tack för du hämta din bil, välkommen åter!");
+
+
         }
         if (choice == 3) {
-            System.out.println("Mata in regnummret!");
+            System.out.println("Mata in regnummret:");
+            scanner.nextLine();
+            System.out.println(scanner.nextLine());
+            System.out.println("Stämmer det? JA/NEJ");
+            scanner.nextLine();
+
+
         }
         if (choice == 4) {
-            System.out.println("Dessa fordon är parkerade i garaget just nu:" + slotsToString());
+            System.out.println("Dessa fordon är parkerade i garaget just nu: " + slotsToString());
         }
 
 
     }
 
+    // Skiver ut vilka regnummer på de fordon som är parkerade i garaget
     private String slotsToString() {
         String output = "";
         for (Vehicle vehicle : slots) {
@@ -72,6 +93,16 @@ public class Garage {
         }
         return output;
     }
+    // Skriver ut regnummer om du vill parkera
+    private String parkToString() {
+        String output = "";
+        for (Vehicle vehicle : park) {
 
+            output += vehicle.licenseNum + " ,";
+
+        }
+        return output;
+    }
+    
 
 }
